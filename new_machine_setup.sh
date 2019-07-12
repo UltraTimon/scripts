32e4360635ce782861d5f0f0550c753b9638f5d0
Timon Bestebreur <timonbestebreur@gmail.com>
2019-07-12 14:33:54 +0200

    Add new script for setting up a new machine
    It's not yet finished

---
+++ new_machine_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# sudo apt-get update
+# sudo apt-get install vim tmux zsh build-essential git-core curl wget
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# git config --global user.name "Timon Bestebreur"
+# git config --global user.email timonbestebreur@gmail.com
+
+# git clone ssh:github.com/UltraTimon/dotfiles.git
+# git clone ssh:githun.com/UltraTimon/scripts.git
+
+echo "todo add:
+- ssh keygen and add to git
+- think about more stuff to do
+- test this stuff"  
+
+echo "Manually install:
+dropbox
+franz
+protonmail bridge
+thunderbird
+"
