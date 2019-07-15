987735f9795da8dd0bd88d8ccafa08fe9f14350b
Timon Bestebreur <timonbestebreur@gmail.com>
2019-07-15 15:02:35 +0200

    Add termdown and concalc to installs

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install vim tmux zsh build-essential git-core curl wget
+sudo apt-get install vim tmux zsh build-essential git-core curl wget concalc termdown
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Set up git
