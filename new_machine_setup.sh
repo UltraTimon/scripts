76f957ba6f55605e5ab278ca2b69fefb7c2e9396
Timon Bestebreur <timonbestebreur@gmail.com>
2019-08-13 20:17:19 +0200

    Add && to line ends to make sure commands wait for the previous command

--- new_machine_setup.sh
+++ new_machine_setup.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
-echo '
-sudo apt-get update
-sudo apt-get install vim tmux zsh build-essential git-core curl wget concalc 
-snap install termdown
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get update &&
+sudo apt-get install vim tmux zsh build-essential git-core curl wget concalc &&
+snap install termdown &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
 
 # Set up git
-git config --global user.name "Timon Bestebreur"
-git config --global user.email timonbestebreur@gmail.com
-git config --global core.editor vim
+git config --global user.name "Timon Bestebreur" &&
+git config --global user.email timonbestebreur@gmail.com &&
+git config --global core.editor vim &&
 
 # Generate ssh key pair
-echo "\n\n Press enter on the following queries\n"
-ssh-keygen -t rsa
-echo "\n\n Copy/paste the following in the github ssh key textbox:\n\n"
-cat ~/.ssh/id_rsa.pub
-echo "\n\n"
+echo "\n\n Press enter on the following queries\n" &&
+ssh-keygen -t rsa &&
+echo "\n\n Copy/paste the following in the github ssh key textbox:\n\n" &&
+cat ~/.ssh/id_rsa.pub &&
+echo "\n\n" &&
 
 # Clone dotfiles and scripts
-git clone ssh:github.com/UltraTimon/dotfiles.git
-git clone ssh:githun.com/UltraTimon/scripts.git
+git clone ssh:github.com/UltraTimon/dotfiles.git &&
+git clone ssh:githun.com/UltraTimon/scripts.git &&
 
 # add the alias and zsh file to the .zshrc file
-echo "source ~/dotfiles/zshrc.zsh" | cat >> .zshrc
-echo "source ~/dotfiles/aliases" | cat >> .zshrc
+echo "source ~/dotfiles/zshrc.zsh" | cat >> .zshrc &&
+echo "source ~/dotfiles/aliases" | cat >> .zshrc &&
 
 # add the vim configs to a .vimrc file
 echo "source ~/dotfiles/vimrc.vim" | cat >> .vimrc
-
-'
 
 echo -e "Manually install/configure:
 dropbox \t\t(https://www.dropbox.com/)
